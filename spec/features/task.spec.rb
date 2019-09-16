d5056e5b317d426b5fee2790544dccdb0bf23f87
PeterYves <iradupeteryves@gmail.com>
2019-09-16 17:57:02 +0200

    step8 corrected without any error

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -5,38 +5,48 @@ require 'rails_helper'
 RSpec.feature "Task management function", type: :feature do
   # In scenario (alias of it), write the processing of the test for each item you want to check.
   scenario "Test task list" do
-    Task.create!(name: 'test_task_01', details: 'testtesttest')
-    Task.create!(name: 'test_task_02', details: 'samplesample')
+    # Create two tasks in advance to use in the task list test
+  Task.create!(name: 'test_task_01', details: 'testtesttest')
+  Task.create!(name: 'test_task_02', details: 'samplesample')
+
+  # visit to tasks_path (transition to task list page)
+  visit tasks_path
+
+  save_and_open_page
   
-    visit tasks_path
-  
-    # Place it where you want to check the actual situation.
-    # In the case of the example, to check "What happens if you go to the task list page after the task is saved"
-    # Save_and_open_page is placed immediately after visit tasks_path
-    save_and_open_page
-  
-    expect(page).to have_content 'Incorrect content 1'
-    expect(page).to have_content 'Incorrect content 2'
+  expect(page).to have_content 'testtesttest'
+  expect(page).to have_content 'samplesample'
   end
 
   scenario "Test task creation" do
-    # visit to new_task_path (transition to task registration page)
-    # 1.Write the process to visit new_task_path here
-  
-    # In the input field labeled "Task Name" and in the input field labeled "Task Details"
-    # Fill in the task title and content respectively
-    # 2.Write the process to fill_in (input) the contents in the input field of label name "task name" here
-    # 3.Write the process to fill_in (input) the contents in the input column of the label name "task details" here
-  
-    # Click_on a button with a value (notation letter) of “Register”
-    # 4.Write a process to click_on (click) a button with a value (notation letter) of “Register”
-  
-    # Check if the information that is supposed to be registered by click is displayed on the task detail page
-    # (Assumption that transition to the task details screen will be made if the task is registered)
-    # 5.On the task detail page, Write a code to check (expect) whether the data (description) that should have been created by the test code is have_content (included)
+     # visit to new_task_path (transition to task registration page)
+   visit new_task_path
+   # In the input field labeled "Task Name" and in the input field labeled "Task Details"
+   # Fill in the task title and content respectively
+   fill_in  'Name' ,  with: 'completed'
+   fill_in  'Detail' ,  with: 'ruby task'
+   # Click_on a button with a value (notation letter) of “Register”
+   click_on  '登録する'
+   # Check if the information that is supposed to be registered by click is displayed on the task detail page
+   # (Assumption that transition to the task details screen will be made if the task is registered)
+   expect(page ).to  have_content  'ruby task'
   end
 
   scenario "Test task details" do
+    task1=Task.create!(name: 'test_task_03', details: 'samplesample')
+    visit task_path(id: task1.id)
 
+    expect(page).to have_content('test_task_03')
+    expect(page).to have_content('samplesample')
+  end
+  scenario "Test task updating" do
+    task1=Task.create!(name: 'test_task_03', details: 'samplesample')
+    visit edit_task_path(id: task1.id)
+    fill_in 'Name', with: 'suredeal'
+    fill_in 'Detail', with: 'of course'
+    click_on '更新する'
+    visit tasks_path
+    expect(page).to have_content('suredeal')
+    expect(page).to have_content('of course')
   end
 end
