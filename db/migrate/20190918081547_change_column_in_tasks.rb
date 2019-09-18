cf16b767ca16fe809785f5bc7d2ce54ee9dcdb21
PeterYves <iradupeteryves@gmail.com>
2019-09-18 10:40:14 +0200

    step12 validations

---
+++ db/migrate/20190918081547_change_column_in_tasks.rb
@@ -0,0 +1,9 @@
+class ChangeColumnInTasks < ActiveRecord::Migration[5.2]
+  def change
+    change_column :tasks, :status, :string, null: false
+    change_column :tasks, :priority, :string, null: false
+    change_column :tasks, :startdate, :datetime, null: false
+    change_column :tasks, :enddate, :datetime, null: false
+    change_column :tasks, :details, :string, null: false
+  end
+end
