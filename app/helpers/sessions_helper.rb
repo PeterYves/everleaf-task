10a545693b654fc890403e00fd0aa814cea270d9
PeterYves <iradupeteryves@gmail.com>
2019-10-04 11:42:24 +0200

    step16 corrected

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,9 @@ module SessionsHelper
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
     
-   def authorize
-     redirect_to new_session_path if current_user.blank?
-   end
+  #  def authorize
+  #    redirect_to new_session_path if current_user.blank?
+  #  end
 
   def logged_in?
     current_user.present?
